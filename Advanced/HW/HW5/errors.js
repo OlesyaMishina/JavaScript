64d54193273e7b4befe9986d1cdb0488b74a69d4
OlesyaMishina <olesyaavilova33@gmail.com>
2024-02-20 10:19:35 +0300

    HW5

---
+++ Advanced/HW/HW5/errors.js
@@ -0,0 +1,22 @@
+const datainfo = `
+ [
+        {
+            "status": "403",
+            "pointer": "pointer: /data/attributes/secretPowers",
+            "image": "img/403.png",
+            "detail": "Editing secret powers is not authorized on Sundays."
+        },
+        {
+            "status": "422",
+            "pointer": "pointer: /data/attributes/volume",
+            "image": "img/422.png",
+            "detail": "Volume does not, in fact, go to 11."
+        },
+        {
+            "status": "500",
+            "pointer": "pointer: /data/attributes/reputation",
+            "image": "img/500.png",
+            "detail": "Reputation service not responding after three requests."
+        }
+    ]
+`
